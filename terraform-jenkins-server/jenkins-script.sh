63d0d7f8d808d80ec351a50aa4bf24801fc5782c
akashveera <akash.veera123@gmail.com>
2024-08-09 12:23:12 +1000

    fixes

--- terraform-jenkins-server/jenkins-script.sh
+++ terraform-jenkins-server/jenkins-script.sh
@@ -23,6 +23,6 @@ sudo yum -y install terraform
 
 # install kubectl
 
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.30.0/bin/linux/amd64/kubectl
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
